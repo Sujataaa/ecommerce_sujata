ce85641e25c4dca77b4ce3f9ba5b0635491594f7
Sujata <bhatta.sujata123@gmail.com>
2022-07-17 10:31:37 +0545

    message

--- product_module/admin.py
+++ product_module/admin.py
@@ -5,20 +5,20 @@ from .models import Brand, Category, Product
 admin.site.register(Brand)
 admin.site.register(Category)
 class ProductAdmin(admin.ModelAdmin):
-list_display = ["name", "price", "brand", "category",]
+    list_display = ["name", "price", "brand", "category",]
 search_fields = ["name", "price", "brand__name", "category__name",]
 list_filter = ["brand","category",]
 readonly_fields = ["quantity",]
 class Meta:
-model = Product
+    model = Product
 admin.site.register(Product, ProductAdmin)
 ...
 class ProductAdmin(admin.ModelAdmin):
-list_display = ["image_tag", "name", "price", "brand", "category",]
+    list_display = ["image_tag", "name", "price", "brand", "category",]
 search_fields = ["name", "price", "brand__name", "category__name",]
 list_filter = ["brand","category","price",]
 # readonly_fields = ["quantity",]
 class Meta:
-model = Product
+    model = Product
 admin.site.register(Product, ProductAdmin)
 
